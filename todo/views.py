fa63b3cbaf4f95675533c6bea288877479e714fa
Godinhoweverson <>
2023-09-25 21:05:03 +0000

    Rendering data

--- todo/views.py
+++ todo/views.py
@@ -5,7 +5,7 @@ from .models import Item
 
 def get_todo_list(request):
     items = Item.objects.all()
-    contect = {
+    context = {
         'items': items
     }
     return render(request, 'todo/todo_list.html', context)
