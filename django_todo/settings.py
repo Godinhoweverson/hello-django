85e6567ff7c8bb497ec571902552bc4e48b2b853
Godinhoweverson <>
2023-09-28 20:10:06 +0000

    Create delete items

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ef-sh0pah7o!^_j$sdcgsv@65#t7rg_6+7sa)vvfgx)i(j6)x8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-godinhoweverson-hello-dj-2fy4mhwwnp.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-godinhoweverson-hello-dj-zdhpqc5txy.us2.codeanyapp.com']
 
 
 # Application definition
