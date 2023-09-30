c625d29bfec40fe1dbe0e4af7ae1f8dc381acaab
Godinhoweverson <>
2023-09-30 11:17:31 +0000

    prepared to deploy to heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -1,3 +1,9 @@
+from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+     import env
+
 """
 Django settings for django_todo project.
 
@@ -25,7 +31,7 @@ SECRET_KEY = 'django-insecure-ef-sh0pah7o!^_j$sdcgsv@65#t7rg_6+7sa)vvfgx)i(j6)x8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-godinhoweverson-hello-dj-c8s16lie23.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-godinhoweverson-hello-dj-51a73aoply.us2.codeanyapp.com']
 
 
 # Application definition
@@ -74,13 +80,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
