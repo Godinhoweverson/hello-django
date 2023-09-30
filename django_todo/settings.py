8660c3842dd9371781de00854cfd49a43370663d
Godinhoweverson <>
2023-09-30 11:37:34 +0000

    deploying

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-ef-sh0pah7o!^_j$sdcgsv@65#t7rg_6+7sa)vvfgx)i(j6)x8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-godinhoweverson-hello-dj-51a73aoply.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-godinhoweverson-hello-dj-yl834tvujt.us2.codeanyapp.com']
 
 
 # Application definition
